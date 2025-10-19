e7b01082f018179dba57262cbdb98622c8026c3f
Martin Knoller Stocker <linuxswords@gmail.com>
2025-10-19 13:37:18 +0200

    release process fix; production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'csvtabler.js',
-    libraryTarget: 'var',
+    libraryTarget: 'umd',
     library: 'csvtabler',
+    globalObject: 'this',
     clean: true
-  }
+  },
+  devtool: 'source-map'
 }
